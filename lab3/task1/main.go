18d609f9b64a3c6bd48985d0fc2b11346c8df5f4
belokmaster <sychev.nickolay1@gmail.com>
2025-02-24 21:15:41 +0300

    add bab alg workly

--- lab3/task1/main.go
+++ lab3/task1/main.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"fmt"
-	"image/color"
 	"math"
 	"math/rand"
 	"time"
-
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/vg"
 )
 
 const (
@@ -20,6 +15,11 @@ type Path struct {
 	cities   []int
 	distance int
 }
+
+var (
+	finalPath []int
+	finalRes  int
+)
 
 func generateDistanceMatrix(n int) [][]int {
 	if n < 2 {
@@ -41,8 +41,26 @@ func generateDistanceMatrix(n int) [][]int {
 		}
 	}
 
+	// Вывод матрицы с подписями городов
 	fmt.Println("Сгенерированная матрица расстояний:")
+
+	// Вывод заголовков (номеров городов сверху)
+	fmt.Printf("    ")
 	for i := 0; i < n; i++ {
+		fmt.Printf("%3d ", i)
+	}
+	fmt.Println()
+
+	// Разделительная линия
+	fmt.Printf("    ")
+	for i := 0; i < n; i++ {
+		fmt.Print("----")
+	}
+	fmt.Println()
+
+	// Вывод строк с номерами городов слева
+	for i := 0; i < n; i++ {
+		fmt.Printf("%2d |", i) // Номер города слева
 		for j := 0; j < n; j++ {
 			fmt.Printf("%3d ", matrix[i][j])
 		}
@@ -50,6 +68,103 @@ func generateDistanceMatrix(n int) [][]int {
 	}
 
 	return matrix
+}
+
+func firstMin(matrix [][]int, i int) int {
+	min := math.MaxInt32
+	for k := 0; k < len(matrix); k++ {
+		if matrix[i][k] < min && i != k {
+			min = matrix[i][k]
+		}
+	}
+	return min
+}
+
+func secondMin(matrix [][]int, i int) int {
+	first, second := math.MaxInt32, math.MaxInt32
+	for j := 0; j < len(matrix); j++ {
+		if i == j {
+			continue
+		}
+		if matrix[i][j] <= first {
+			second = first
+			first = matrix[i][j]
+		} else if matrix[i][j] <= second && matrix[i][j] != first {
+			second = matrix[i][j]
+		}
+	}
+	return second
+}
+
+func copyToFinal(currPath []int, n int) {
+	finalPath = make([]int, n+1)
+	copy(finalPath, currPath)
+	finalPath[n] = currPath[0]
+}
+
+func TSPRec(matrix [][]int, currBound, currWeight, level int, currPath []int, visited []bool) {
+	n := len(matrix)
+
+	if level == n {
+		if matrix[currPath[level-1]][currPath[0]] != 0 {
+			currRes := currWeight + matrix[currPath[level-1]][currPath[0]]
+			if currRes < finalRes {
+				copyToFinal(currPath, n)
+				finalRes = currRes
+			}
+		}
+		return
+	}
+
+	for i := 0; i < n; i++ {
+		if matrix[currPath[level-1]][i] != 0 && !visited[i] {
+			temp := currBound
+			currWeight += matrix[currPath[level-1]][i]
+
+			if level == 1 {
+				currBound -= (firstMin(matrix, currPath[level-1]) + firstMin(matrix, i)) / 2
+			} else {
+				currBound -= (secondMin(matrix, currPath[level-1]) + firstMin(matrix, i)) / 2
+			}
+
+			if currBound+currWeight < finalRes {
+				currPath[level] = i
+				visited[i] = true
+				TSPRec(matrix, currBound, currWeight, level+1, currPath, visited)
+			}
+
+			currWeight -= matrix[currPath[level-1]][i]
+			currBound = temp
+
+			visited = make([]bool, n)
+			for j := 0; j < level; j++ {
+				if currPath[j] != -1 {
+					visited[currPath[j]] = true
+				}
+			}
+		}
+	}
+}
+
+func TSP(matrix [][]int) Path {
+	n := len(matrix)
+	currBound := 0
+	currPath := make([]int, n+1)
+	visited := make([]bool, n)
+
+	for i := 0; i < n; i++ {
+		currBound += (firstMin(matrix, i) + secondMin(matrix, i))
+	}
+
+	currBound = int(math.Ceil(float64(currBound) / 2))
+
+	visited[0] = true
+	currPath[0] = 0
+
+	finalRes = math.MaxInt32
+	TSPRec(matrix, currBound, 0, 1, currPath, visited)
+
+	return Path{cities: finalPath, distance: finalRes}
 }
 
 func greedyTSP(matrix [][]int, start int) Path {
@@ -83,90 +198,6 @@ func greedyTSP(matrix [][]int, start int) Path {
 	return Path{cities: path, distance: totalDistance}
 }
 
-func calculateLowerBound(matrix [][]int, path []int, n int) int {
-	lowerBound := 0
-
-	// Добавляем расстояния для уже выбранных рёбер
-	for i := 0; i < len(path)-1; i++ {
-		lowerBound += matrix[path[i]][path[i+1]]
-	}
-
-	// Добавляем минимальные рёбра для оставшихся городов
-	visited := make([]bool, n)
-	for _, city := range path {
-		visited[city] = true
-	}
-
-	// Для каждого города, который ещё не был посещен, находим минимальное ребро
-	for i := 0; i < n; i++ {
-		if !visited[i] {
-			minEdge := math.MaxInt32
-			for j := 0; j < n; j++ {
-				if !visited[j] && i != j && matrix[i][j] < minEdge {
-					minEdge = matrix[i][j]
-				}
-			}
-			lowerBound += minEdge
-		}
-	}
-
-	return lowerBound
-}
-
-func branchAndBoundTSP(matrix [][]int) Path {
-	n := len(matrix)
-	bestPath := make([]int, n+1)
-	bestDistance := math.MaxInt32
-
-	// Используем стек для хранения состояний
-	type State struct {
-		path            []int
-		currentDistance int
-	}
-	stack := []State{{path: []int{0}, currentDistance: 0}}
-
-	for len(stack) > 0 {
-		currentState := stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
-
-		if len(currentState.path) == n {
-			currentState.currentDistance += matrix[currentState.path[len(currentState.path)-1]][currentState.path[0]]
-			if currentState.currentDistance < bestDistance {
-				bestDistance = currentState.currentDistance
-				copy(bestPath, append(currentState.path, currentState.path[0]))
-			}
-			continue
-		}
-
-		lastCity := currentState.path[len(currentState.path)-1]
-		for nextCity := 0; nextCity < n; nextCity++ {
-			if !contains(currentState.path, nextCity) {
-				newPath := append(currentState.path, nextCity)
-				newDistance := currentState.currentDistance + matrix[lastCity][nextCity]
-
-				// Вычисляем нижнюю границу
-				lowerBound := calculateLowerBound(matrix, newPath, n)
-
-				// Если нижняя граница меньше лучшего расстояния, продолжаем поиск
-				if lowerBound < bestDistance {
-					stack = append(stack, State{path: newPath, currentDistance: newDistance})
-				}
-			}
-		}
-	}
-
-	return Path{cities: bestPath, distance: bestDistance}
-}
-
-func contains(path []int, city int) bool {
-	for _, c := range path {
-		if c == city {
-			return true
-		}
-	}
-	return false
-}
-
 func generateAndSolveTSP() {
 	if numCities < 2 {
 		fmt.Println("Количество городов должно быть хотя бы 2.")
@@ -187,7 +218,7 @@ func generateAndSolveTSP() {
 	fmt.Printf("\nОбщее расстояние: %d\n", greedyResult.distance)
 
 	// Решаем задачу методом ветвей и границ
-	branchAndBoundResult := branchAndBoundTSP(matrix)
+	branchAndBoundResult := TSP(matrix)
 
 	fmt.Printf("\nРешение задачи коммивояжёра (метод ветвей и границ):\n")
 	fmt.Printf("Путь: ")
@@ -195,89 +226,6 @@ func generateAndSolveTSP() {
 		fmt.Printf("%d ", city)
 	}
 	fmt.Printf("\nОбщее расстояние: %d\n", branchAndBoundResult.distance)
-
-	// Визуализация графа
-	visualizeGraph(matrix, branchAndBoundResult.cities)
-}
-
-func randomColor() color.RGBA {
-	return color.RGBA{
-		R: uint8(rand.Intn(256)),
-		G: uint8(rand.Intn(256)),
-		B: uint8(rand.Intn(256)),
-		A: 255,
-	}
-}
-
-func visualizeGraph(matrix [][]int, path []int) {
-	p := plot.New()
-
-	pts := make(plotter.XYs, numCities)
-	pointColors := make([]color.RGBA, numCities)
-	for i := 0; i < numCities; i++ {
-		pts[i].X = float64(rand.Intn(100))
-		pts[i].Y = float64(rand.Intn(100))
-		pointColors[i] = randomColor()
-	}
-
-	scatterData := make(plotter.XYs, numCities)
-	for i := 0; i < numCities; i++ {
-		scatterData[i] = pts[i]
-	}
-	line, err := plotter.NewScatter(scatterData)
-	if err != nil {
-		fmt.Println("Ошибка при добавлении точек на граф:", err)
-		return
-	}
-
-	for i := range scatterData {
-		line.GlyphStyle.Color = pointColors[i]
-	}
-
-	line.GlyphStyle.Radius = vg.Points(5)
-	p.Add(line)
-
-	for i := 0; i < numCities; i++ {
-		for j := i + 1; j < numCities; j++ {
-			if matrix[i][j] > 0 {
-				lines := plotter.XYs{
-					{X: pts[i].X, Y: pts[i].Y},
-					{X: pts[j].X, Y: pts[j].Y},
-				}
-
-				linePlot, err := plotter.NewLine(lines)
-				if err != nil {
-					fmt.Println("Ошибка при создании линии:", err)
-					return
-				}
-				linePlot.Color = color.Black
-				p.Add(linePlot)
-			}
-		}
-	}
-
-	for i := 0; i < len(path)-1; i++ {
-		lines := plotter.XYs{
-			{X: pts[path[i]].X, Y: pts[path[i]].Y},
-			{X: pts[path[i+1]].X, Y: pts[path[i+1]].Y},
-		}
-
-		linePlot, err := plotter.NewLine(lines)
-		if err != nil {
-			fmt.Println("Ошибка при создании линии для пути:", err)
-			return
-		}
-		linePlot.Color = color.Black
-		p.Add(linePlot)
-	}
-
-	err = p.Save(10*vg.Inch, 10*vg.Inch, "graph.png")
-	if err != nil {
-		fmt.Println("Ошибка при сохранении графа:", err)
-		return
-	}
-
-	fmt.Println("Граф с путями сохранён в файл graph.png")
 }
 
 func main() {
